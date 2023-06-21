7dbea95209c93d94fb984520959dea2e8df0daee
vishalicious213 <vishalicious213@gmail.com>
2023-06-21 15:00:46 -0700

    styled Card

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,5 +1,13 @@
+import katie from "/katie-zaferes.png"
+import star from "/star.png"
+
 export default function Card() {
     return (
-        <h1>CARD</h1>
+        <div className="card">
+            <img className="experience-pic" src={katie} />
+            <p><img className="star" src={star}/>5.0 <span className="gray-text">(6) • USA</span></p>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><b>From $136</b> / person</p>
+        </div>
     )
 }
