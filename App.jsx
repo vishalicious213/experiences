2488c13cef880d924da410761026e780b906670a
vishalicious213 <vishalicious213@gmail.com>
2023-06-21 14:42:27 -0700

    drafted /components/Card.jsx and rendered it via App

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 
 export default function App() {
     return (
         <>
             <Navbar />
             <Hero />
+            <Card />
         </>
     )
 }
