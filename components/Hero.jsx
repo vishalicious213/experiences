a8fab326a9f902de546d28f365449cbd56cead9f
vishalicious213 <vishalicious213@gmail.com>
2023-06-21 14:10:11 -0700

    added spacing around Hero img

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import hero from "/hero.png"
 
 export default function Hero() {
     return (
-        <section className="hero">
+        <section className="hero container">
             <img src={hero} alt="tiled gallery of airbnb experiences" />
             <h1>Online Experiences</h1>
             <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
