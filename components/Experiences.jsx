b13867f9493534e70449f1bcfb8f3c5a10a92057
vishalicious213 <vishalicious213@gmail.com>
2023-06-22 11:18:44 -0700

    rendering /data.js info from Experiences => Card

--- components/Experiences.jsx
+++ components/Experiences.jsx
@@ -1,15 +1,24 @@
 import Card from "./Card"
-import katie from "/katie-zaferes.png"
+import data from "../data"
 
 export default function Experiences() {
+    const elements = data.map(item => {
+        return (
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })
+
     return (
-        <Card 
-            img={katie}
-            rating="5.0"
-            reviewCount={6}
-            country="USA"
-            title="Life lessons with Katie Zaferes"
-            price={136}
-        />
+        <div>
+            {elements}
+        </div>
+        
     )
 }
