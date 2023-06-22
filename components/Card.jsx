9cf159ec86a6a70841ab14e26c195bf318879702
vishalicious213 <vishalicious213@gmail.com>
2023-06-22 10:41:43 -0700

    updated Card to take in location prop instead of country prop

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,10 +1,10 @@
 import star from "/star.png"
 
-export default function Card({img, rating, reviewCount, country, title, price}) {
+export default function Card({img, rating, reviewCount, location, title, price}) {
     return (
         <div className="card">
             <img className="experience-pic" src={img} />
-            <p><img className="star" src={star}/>{rating} <span className="gray-text">({reviewCount}) • {country}</span></p>
+            <p><img className="star" src={star}/>{rating} <span className="gray-text">({reviewCount}) • {location}</span></p>
             <p>{title}</p>
             <p><b>From ${price}</b> / person</p>
         </div>
