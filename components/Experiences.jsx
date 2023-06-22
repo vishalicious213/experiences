58956c3989c09140c5e27443141c1dc268919c6e
vishalicious213 <vishalicious213@gmail.com>
2023-06-21 22:30:42 -0700

    drafted /components/Experiences.jsx

---
+++ components/Experiences.jsx
@@ -0,0 +1,14 @@
+import Card from "./Card"
+
+export default function Experiences() {
+    return (
+        <Card 
+            img="katie-zaferes.png"
+            rating="5.0"
+            reviewCount={6}
+            country="USA"
+            title="Life Lessons with Katie Zaferes"
+            price={136}
+        />
+    )
+}
