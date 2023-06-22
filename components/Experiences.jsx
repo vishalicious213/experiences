f23fc486e5801c7d79fe63fc54e6bf0fed34c6e3
vishalicious213 <vishalicious213@gmail.com>
2023-06-22 11:25:03 -0700

    added key to each item being rendered by Card in Experiences

--- components/Experiences.jsx
+++ components/Experiences.jsx
@@ -5,6 +5,7 @@ export default function Experiences() {
     const elements = data.map(item => {
         return (
             <Card 
+                key={item.id}
                 img={item.coverImg}
                 rating={item.stats.rating}
                 reviewCount={item.stats.reviewCount}
