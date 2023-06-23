e114a589eb69f5df5a27e5d29f9bffa53c976531
vishalicious213 <vishalicious213@gmail.com>
2023-06-22 17:33:39 -0700

    added falconry to experiences

--- data.js
+++ data.js
@@ -1,6 +1,32 @@
 export default [
     {
         id: 1,
+        title: "Learn to Eat Fire",
+        description: "Cat will take you through the theory and safety precautions behind fire eating and body burning. We'll then actually light up the torches and explore this performance art fully with hand transfers, tongue transfers and of course, extinguishing the flame with your mouth.",
+        price: 65,
+        coverImg: "eat-fire.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 177
+        },
+        location: "North Hollywood",
+        openSpots: 3,
+    },
+    {
+        id: 5,
+        title: "Falconry over the beach",
+        description: "You will help us free fly our falcons on the cliffs of La Jolla at the Torrey Pines Gliderport, a National Historical Place and beloved landmark.",
+        price: 77,
+        coverImg: "falconry.jpg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 722
+        },
+        location: "San Diego",
+        openSpots: 3,
+    },
+    {
+        id: 2,
         title: "Life lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
@@ -13,7 +39,7 @@ export default [
         openSpots: 0,
     },
     {
-        id: 2,
+        id: 3,
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
@@ -26,7 +52,7 @@ export default [
         openSpots: 27,
     },
     {
-        id: 3,
+        id: 4,
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
@@ -38,17 +64,4 @@ export default [
         location: "Norway",
         openSpots: 3,
     },
-    {
-        id: 4,
-        title: "Learn to Eat Fire",
-        description: "Cat will take you through the theory and safety precautions behind fire eating and body burning. We'll then actually light up the torches and explore this performance art fully with hand transfers, tongue transfers and of course, extinguishing the flame with your mouth.",
-        price: 65,
-        coverImg: "eat-fire.jpg",
-        stats: {
-            rating: 5.0,
-            reviewCount: 177
-        },
-        location: "North Hollywood",
-        openSpots: 3,
-    }
 ]
